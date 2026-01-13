b086fbcb32860bcd4a5fa5947e04857a4c005a3a
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2026-01-13 11:17:53 +0530

    done

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import ENV from './lib/env.js';
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+console.log(ENV.DB_URL);
+app.listen(ENV.PORT,()=>{
+    console.log("server is running on port"+" " + `${ENV.PORT}`);
+})
+
