46f3ff98444c9bb4b32f3f9d27f8c7fd443962c8
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2026-01-14 09:02:05 +0530

    deployment config edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import ENV from './lib/env.js';
-
+import path from 'path';
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+const __dirname = path.resolve();
+app.get('/home',(req,res)=>{
+    res.send("Hello chulbul pandey kya kr rhi ho");
 })
+
+app.get('/hotel',(req,res)=>{
+    res.send("Welcome to Hotel");
+})
+
+console.log(ENV.NODE_ENV);
+
+if(ENV.NODE_ENV === "xyz"){
+    app.use(express.static(path.join(__dirname,'../frontend','vite-project','dist')))
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend','vite-project','dist','index.html'))
+    })
+}
 
 console.log(ENV.DB_URL);
 console.log(ENV.PORT);
