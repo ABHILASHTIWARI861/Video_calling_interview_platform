1d81141fdfbb9641f98987b761b1312591529b4c
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2026-01-17 18:36:43 +0530

    Auth_Stream_Sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,31 @@
+import {Streamchat} from 'stream-chat';
+import ENV from './env';
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    throw new Error("STREAM_API_KEY and STREAM_API_SECRET are required");;
+}
+
+export const chatClient = Streamchat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser = async(userData) =>{
+  try{
+  await chatClient.upsertUser(userData);
+  console.log("Stream user created or updated successfully:",userData);
+  }
+  catch(error){
+    console.log("Error in creating stream user token",error);
+}
+}
+ 
+export const deleteStreamUser = async(userId) =>{
+  try{
+  await chatClient.deleteUser(userId);
+  console.log("Stream user deleted successfully:",userId);
+  }
+  catch(error){
+    console.log("Error in deleting stream user token",error);
+}
+}
